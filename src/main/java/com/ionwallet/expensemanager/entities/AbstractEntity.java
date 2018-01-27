f552bad569ed85adb5a53aa7949df2439a298661
IN\mukul.lakhwani <muku.lakh@gmail.com>
2018-01-28 01:44:43 +0530

    common chnages

--- src/main/java/com/ionwallet/expensemanager/entities/AbstractEntity.java
+++ src/main/java/com/ionwallet/expensemanager/entities/AbstractEntity.java
@@ -1,13 +1,12 @@
 package com.ionwallet.expensemanager.entities;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 import javax.persistence.Column;
+import javax.persistence.Convert;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.persistence.Version;
 
 import org.springframework.data.annotation.CreatedBy;
@@ -16,22 +15,26 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.util.StringUtils;
 
+import com.ionwallet.expensemgrutility.converters.LocalDateTimeAttributeConverter;
+
+
+
 @MappedSuperclass
 public class AbstractEntity {
 
 	@CreatedDate
-	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "CREATED_TS")
-	protected Date createdTime;
+	@Convert(converter=LocalDateTimeAttributeConverter.class)
+	protected LocalDateTime createdTime;
 
 	@CreatedBy
 	@Column(name = "CREATED_BY")
 	protected String createdBy;
 
 	@LastModifiedDate
-	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "UPDATED_TS")
-	protected Date updatedTime;
+	@Convert(converter=LocalDateTimeAttributeConverter.class)
+	protected LocalDateTime updatedTime;
 
 	@LastModifiedBy
 	@Column(name = "UPDATED_BY")
@@ -49,7 +52,7 @@ public class AbstractEntity {
 		} else if (StringUtils.isEmpty(this.createdBy)) {
 			this.createdBy = "1";
 		}
-		this.createdTime = new Date();
+		this.createdTime = LocalDateTime.now();
 		this.updatedBy = null;
 		this.updatedTime = null;
 		}
@@ -63,14 +66,14 @@ public class AbstractEntity {
 		else if (StringUtils.isEmpty(this.updatedBy)) {
 			this.updatedBy = "1";
 		}
-		this.updatedTime = new Date();
+		this.updatedTime = LocalDateTime.now();
 	}
 
-	public Date getCreatedTime() {
+	public LocalDateTime getCreatedTime() {
 		return createdTime;
 	}
 
-	public void setCreatedTime(Date createdTime) {
+	public void setCreatedTime(LocalDateTime createdTime) {
 		this.createdTime = createdTime;
 	}
 
@@ -82,11 +85,11 @@ public class AbstractEntity {
 		this.createdBy = createdBy;
 	}
 
-	public Date getupdatedTime() {
+	public LocalDateTime getupdatedTime() {
 		return updatedTime;
 	}
 
-	public void setupdatedTime(Date updatedTime) {
+	public void setupdatedTime(LocalDateTime updatedTime) {
 		this.updatedTime = updatedTime;
 	}
 
